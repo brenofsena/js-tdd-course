6c97d72db66e66cad2450a9b68ecac67c7235302
Breno <breno.fsena@gmail.com>
2020-03-31 01:52:41 -0300

    Add tests

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,18 +2,23 @@ module.exports = {
   env: {
     browser: true,
     es6: true,
+    mocha: true
   },
-  extends: [
-    'airbnb-base',
-  ],
+  plugins: ['mocha'],
+  extends: ['airbnb-base', 'plugin:mocha/recommended'],
   globals: {
     Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly',
+    SharedArrayBuffer: 'readonly'
   },
   parserOptions: {
     ecmaVersion: 2018,
-    sourceType: 'module',
+    sourceType: 'module'
   },
   rules: {
-  },
+    'mocha/no-mocha-arrows': 0,
+    'comma-dangle': ['error', 'only-multiline'],
+    'no-unused-expressions': 0,
+    'object-curly-newline': 0,
+    'arrow-parens': 0
+  }
 };
